b518c9d8c583abcfbd4404e972db1734e9515f95
Jeevan Pillay <jeevanpillay@protonmail.com>
2021-10-02 23:44:43 +1000

    creating flow2 canvas

--- core/flow2.js
+++ core/flow2.js
@@ -4,18 +4,15 @@ const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
 const settings = {
-  dimensions: [1000, 1000]
+  dimensions: [2048, 2048]
 };
 
 const sketch = () => {
-  // 12314213242
-  random.setSeed(12314213242);
-  const colorCount = random.rangeFloor(2, 4);
+  const colorCount = random.rangeFloor(2, 6);
   const palette = random.shuffle(random.pick(palettes)).splice(0, colorCount);
 
   const COUNT = 125;
   const PARTITIONS = 1;
-  const margin = 10 ** 2;
 
   const createGrid = () => {
     let points = [];
@@ -35,25 +32,29 @@ const sketch = () => {
     return points;
   };
 
-  const points = createGrid().filter(() => random.value() > 0.25);
+  const points = createGrid().filter(() => random.value() > 0.75);
+  const margin = 150;
 
   return ({context, width, height}) => {
     context.fillStyle = random.pick(palette);
     context.fillRect(0, 0, width, height);
 
     var res = 20;
+
     points.forEach(data => {
       const {position, angle, color} = data;
       const [u, v] = position;
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
+
       context.save();
       context.translate(x, y);
       context.rotate(angle);
       context.beginPath();
       context.moveTo(0, 0);
-      context.font = `${20}px "Helvetica"`;
+      // context.lineTo(res, 0);
+      context.font = `${30}px "Helvetica"`;
       context.fillStyle = color;
       context.fillText("-", 0, 0);
       context.fill();
