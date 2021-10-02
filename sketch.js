7ed65cec5dff587e7950805d9bd49e64d5d9f473
Jeevan Pillay <jeevanpillay@protonmail.com>
2021-10-02 17:38:30 +1000

    added rotation + radius through noise2D

--- sketch.js
+++ sketch.js
@@ -23,10 +23,12 @@ const sketch = () => {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
 
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
         points.push({
           color: random.pick(palette),
           position: [ u, v ],
-          radius: Math.abs(0.01 + random.gaussian() * 0.01)
+          rotation: random.noise2D(u, v) * 0.5,
+          radius,
         });
       }
     }
@@ -43,17 +45,25 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     points.forEach((point) => {
-      const { position, radius, color } = point;
+      const { position, radius, color, rotation } = point;
 
       const [ u, v ] = position
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('=', 0, 0);
+      context.restore();
     });
   };
 };
