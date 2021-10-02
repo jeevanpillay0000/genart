84b128bf382d4730489d263541676fb2793f2653
Jeevan Pillay <jeevanpillay@protonmail.com>
2021-10-02 16:59:06 +1000

    created grid w circles

--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 2048, 2048 ],
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    let points = [];
+
+    const count = 6;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+
+        points.push([ u, v ]);
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid();
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'orange';
     context.fillRect(0, 0, width, height);
+
+    points.forEach(([ u, v ]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 40, 0, Math.PI * 2);
+      context.strokeStyle = 'red';
+      context.lineWidth = 10;
+      context.stroke();
+    });
   };
 };
 
